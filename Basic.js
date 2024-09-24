68c7786d1ba9b68133f29da9318f3438828f5d54
awadepavan <132901256+awadepavan@users.noreply.github.com>
2024-09-24 10:31:09 +0530

    Questions

---
+++ Basic.js
@@ -0,0 +1,32 @@
+/*
+
+
+1) Waht is javascript ? what is the role of javaScript engine? // v.imp
+
+--> javascript is a programming language that used for converting static web pages to interactivs dynamic web pages.
+
+
+javascript engine is a programe in ewb browser that excutes javascript code
+
+2) what sre Client Side and Server Side ?
+-->  Client is a device, application or software component that requests and consumes services or resources from server
+
+
+3) what is scope in javascripe?
+--> Scope define where variable defined and where   they  can be anywhere
+      There are three types of scope 
+      1) Global Scope :- Access Anywhere
+      2) Block Scope :- Accept inside the block  Means Access only in block
+      3) function scope :- Access inside the function Anywhere
+
+4)  What is the type of variables in js when it is declared without using the var,let,or const keyword?
+-->  "var" is the implicite type of variables when a variable is not define with the var,let and const
+
+5) What is Hoisting in javaScript?
+-->   hoisting is a javasccript behavior  where # **functions and variable**  declaration move to the top of there respective scopes during the compition phase
+
+6) what is JSON?
+-->  JSON :- (Javascript Object Notation) is a lightweight data interchange formate.
+
+JSON contains of key-value pairs.
+
---
+++ ch_2_datatypes and variables.js
@@ -0,0 +1,33 @@
+/* 
+1)  what are variables ? what is the diff var let const ?
+--->
+  variables :- Means its a container to store the data in it
+
+
+  Diff between var,let,const
+
+  var:- var creates a function scope variables
+  let :- let creates a block- scope variables
+  const:- we an assign value only once , and value cant be changed
+
+
+2)  what are data types in js?
+--->  
+ A data type is determines the type of variables
+ 
+ types :-
+  (simple data type)
+ 1)primitive:- Number,Boolean,Bigint , String,Null,undefine
+ 2)Non-primitive:- Array,Obj,Function,undefined,Null
+    (Complex data types)
+3) what is thhe diff between primitivee and non-primitive data types?
+--> primitive :- it can hold only  sigle datatype
+                it is immutable means after asssigning the value cant be changed
+
+    Non-primitive:-It can hold many values 
+                these are not immutable means after assigning value we can channge vallue also
+
+
+
+
+*/
